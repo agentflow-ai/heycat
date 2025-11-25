9c93ba9f6620df81293782f2c44f28f1c5eb5bd3
MichaelHindley <michael@hindley.se>
2025-11-25 15:20:48 +0100

    WIP: Update src-tauri/src/main.rs - Add file-level #[coverage(off)]

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// Exclude entire file from coverage - binary entry point cannot be unit tested
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+#![cfg_attr(coverage_nightly, coverage(off))]
+
 fn main() {
     heycat_lib::run()
 }
